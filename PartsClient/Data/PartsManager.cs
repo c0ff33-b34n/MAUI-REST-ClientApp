990b818c43037730da261be554f23c6283775663
Stuart Bowes <stuartbowes2000@gmail.com>
2022-06-14 14:54:08 +0100

    Add Delete method to delete item from database

--- PartsClient/Data/PartsManager.cs
+++ PartsClient/Data/PartsManager.cs
@@ -82,13 +82,19 @@ namespace PartsClient.Data
             var response = await client.SendAsync(msg);
             response.EnsureSuccessStatusCode();
 
-
-
         }
 
         public static async Task Delete(string partID)
         {
-            throw new NotImplementedException();                        
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                return;
+
+            HttpRequestMessage msg = new(HttpMethod.Delete, $"{Url}parts/{partID}");
+
+            HttpClient client = await GetClient();
+
+            var response = await client.SendAsync(msg);
+            response.EnsureSuccessStatusCode();
         }
     }
 }
